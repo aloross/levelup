07da516de2027416e28ed65b94b155d41d3e31b1
Adrien Louis-Rossignol <adrien.louis.r@gmail.com>
2019-07-02 20:24:25 +0200

    Add bootsrap and basic navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import scss from "../scss/app.scss";
+import "bootstrap";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -9,7 +10,7 @@ import css from "../css/app.css"
 //
 // Import dependencies
 //
-import "phoenix_html"
+import "phoenix_html";
 
 // Import local files
 //
